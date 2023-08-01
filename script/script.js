ec4b6de8c0f1ed8f11bdab1a3a86ef6c18b0b59f
MudHolland <103423480+MudHolland@users.noreply.github.com>
2023-08-01 19:15:22 +0200

    half-width optimized
    js optimized and half-width removed from showcases-html

--- script/script.js
+++ script/script.js
@@ -1,74 +1,66 @@
 // Wrap the code in an immediately-invoked function expression (IIFE) to avoid polluting the global scope
 (function () {
-    // Get the modal, modal image, and caption elements
-    const modal = document.getElementById('myModal');
-    const modalImg = document.getElementById("img01");
-    const captionText = document.getElementById("caption");
-  
-    // Function to handle the click event for images
-    function openModal(img) {
+  // Function to handle the click event for images
+  function openModal(img) {
+    if (modal) {
       modal.style.display = "block";
       modalImg.src = img.src;
       captionText.innerHTML = img.alt;
     }
-  
-    // Get all the image elements on the page and attach click event listeners
-    const images = document.querySelectorAll('img');
-    images.forEach(function (img) {
-      img.addEventListener('click', function () {
-        openModal(this);
-      });
+  }
+
+  // Get all the image elements on the page and attach click event listeners
+  const images = document.querySelectorAll('img');
+  images.forEach(function (img) {
+    img.addEventListener('click', function () {
+      openModal(this);
     });
-  
-    // Function to close the modal
-    function closeModal() {
+  });
+
+  // Function to close the modal
+  function closeModal() {
+    if (modal) {
       modal.style.display = "none";
     }
-  
-    // Get the elements that close the modal and listen for clicks
-    const span = document.getElementsByClassName("close")[0];
-    const screen = document.getElementsByClassName("modal")[0];
-  
+  }
+
+  // Get the elements that close the modal and listen for clicks
+  const modal = document.getElementById('myModal');
+  const modalImg = modal ? document.getElementById("img01") : null;
+  const captionText = modal ? document.getElementById("caption") : null;
+
+  const span = document.getElementsByClassName("close")[0];
+  const screen = document.getElementsByClassName("modal")[0];
+
+  if (span) {
     span.onclick = closeModal;
+  }
+
+  if (screen) {
     screen.onclick = closeModal;
-  
-    // Handle accordion functionality
-    function toggleAccordion(button) {
-      const accordionContent = button.nextElementSibling;
-      button.classList.toggle('accordion__button--active');
-      accordionContent.style.maxHeight = button.classList.contains('accordion__button--active') ? accordionContent.scrollHeight + 'px' : 0;
-    }
-  
-    // Get all the accordion buttons on the page and attach click event listeners
-    const accordionButtons = document.querySelectorAll('.accordion__button');
-    accordionButtons.forEach(function (button) {
-      button.addEventListener('click', function () {
-        toggleAccordion(this);
-      });
-    });
-  
-    // Function to handle image aspect ratio
-    function handleImageAspectRatio(figureElement) {
-      const imgElement = figureElement.querySelector('img');
-      const image = new Image();
-  
-      // Get the natural dimensions of the image without triggering a load event
-      image.onload = function () {
-        if (image.height > image.width) {
-          figureElement.classList.add('half-width');
-        } else {
-          figureElement.classList.remove('half-width');
-        }
-      };
-  
-      // Set the source after the onload event is attached
-      image.src = imgElement.src;
-    }
-  
-    // Get all figure elements on the page and handle image aspect ratio for each one
-    const figureElements = document.querySelectorAll('figure');
-    figureElements.forEach(function (figureElement) {
-      handleImageAspectRatio(figureElement);
-    });
-  })();
-  
+  }
+
+  // Function to handle image aspect ratio
+  function handleImageAspectRatio(figureElement) {
+    const imgElement = figureElement.querySelector('img');
+    const image = new Image();
+
+    // Get the natural dimensions of the image without triggering a load event
+    image.onload = function () {
+      if (image.height > image.width) {
+        figureElement.classList.add('half-width');
+      } else {
+        figureElement.classList.remove('half-width');
+      }
+    };
+
+    // Set the source after the onload event is attached
+    image.src = imgElement.src;
+  }
+
+  // Get all figure elements on the page and handle image aspect ratio for each one
+  const figureElements = document.querySelectorAll('figure');
+  figureElements.forEach(function (figureElement) {
+    handleImageAspectRatio(figureElement);
+  });
+})();
