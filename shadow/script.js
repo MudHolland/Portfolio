0f8a3c5d48c83d209a03258008ee2314572cec44
MudHolland <103423480+MudHolland@users.noreply.github.com>
2023-08-08 14:42:10 +0200

    Update script.js

--- shadow/script.js
+++ shadow/script.js
@@ -1,22 +1,11 @@
 const card = document.getElementById('card');
 
-card.addEventListener('mousemove', (e) => {
-  const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-  const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-
-  card.style.boxShadow = `${xAxis}px ${yAxis}px 10px rgba(0, 0, 0, 0.5)`;
-});
-
-card.addEventListener('mouseleave', () => {
-  card.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.2)';
-});
-
 // Function to update box shadow based on gyroscope/accelerometer data
 function updateBoxShadow(event) {
   const xAxis = event.beta / 10; // Adjust the division factor for sensitivity
   const yAxis = -event.gamma / 10; // Adjust the division factor for sensitivity
 
-  card.style.boxShadow = `${xAxis}px ${yAxis}px 10px rgba(0, 0, 0, 0.5)`;
+  card.style.boxShadow = `${xAxis}px ${yAxis}px 10px rgba(0, 0, 0, 0.2)`;
 }
 
 // Check if the device supports gyroscope/accelerometer
@@ -29,5 +18,5 @@ if (window.DeviceOrientationEvent) {
 
 // Reset box shadow when user touches the card
 card.addEventListener('touchstart', () => {
-  card.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.5)';
+  card.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.2)';
 });
