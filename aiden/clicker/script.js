db35e82a4df67bcf570b0157b7570b29b5c4cd6e
MudHolland <103423480+MudHolland@users.noreply.github.com>
2023-11-18 20:06:08 +0100

    update disabled buttons

--- aiden/clicker/script.js
+++ aiden/clicker/script.js
@@ -5,13 +5,22 @@ let npc = 1;
 let item1 = 0;
 let item2 = 0;
 let item3 = 0;
+let item4 = 0;
+let costItem1 = 40;
+let costItem2 = 150;
+let costItem3 = 500;
+let costItem4 = 500000;
+let cpsItem1 = 100;
+let cpsItem2 = 200;
+let cpsItem3 = 1000;
+let cpsItem4 = 10000;
+let cpsItem5 = 1000000;
 
 // Function to handle increment button click
 function handleIncrement() {
-    npc = (1 + 1 * item1 + 7 * item2 + 40 * item3);
-    document.getElementById("npc").innerText = npc;
     number += npc;
     updateNumber();
+    updateBuyButtons();
 }
 
 // Function to update the displayed number
@@ -21,51 +30,173 @@ function updateNumber() {
 
 // Function to increase CPS
 function increaseCPS1() {
-    if (number >= 100) {
-        number -= 100;
+    if (number >= cpsItem1) {
+        number -= cpsItem1;
         cps += 1;
-        document.getElementById("cps").innerText = cps;
+        updateCPS();
         updateNumber();
+        updateBuyButtons();
     }
 }
 
 function increaseCPS2() {
-    if (number >= 200) {
-        number -= 200;
+    if (number >= cpsItem2) {
+        number -= cpsItem2;
         cps += 3;
-        document.getElementById("cps").innerText = cps;
+        updateCPS();
         updateNumber();
+        updateBuyButtons();
     }
 }
 
+function increaseCPS3() {
+    if (number >= cpsItem3) {
+        number -= cpsItem3;
+        cps += 10;
+        updateCPS();
+        updateNumber();
+        updateBuyButtons();
+    }
+}
+
+function increaseCPS4() {
+    if (number >= cpsItem4) {
+        number -= cpsItem4;
+        cps += 25;
+        updateCPS();
+        updateNumber();
+        updateBuyButtons();
+    }
+}
+
+function increaseCPS2() {
+    if (number >= cpsItem5) {
+        number -= cpsItem5;
+        cps += 3;
+        updateCPS();
+        updateNumber();
+        updateBuyButtons();
+    } 
+}
+
 function buyItem1() {
-    if (number >= 40) {
-        number -= 40;
+    if (number >= costItem1) {
+        number -= costItem1;
         item1 += 1;
         updateNumber();
+        updateNPC();
         updateItem1();
+        updateBuyButtons();
     }
 }
 
 function buyItem2() {
-    if (number >= 200) {
-        number -= 200;
+    if (number >= costItem2) {
+        number -= costItem2;
         item2 += 1;
         updateNumber();
+        updateNPC();
         updateItem2();
+        updateBuyButtons();
     }
 }
 
 function buyItem3() {
-    if (number >= 1000) {
-        number -= 1000;
+    if (number >= costItem3) {
+        number -= costItem3;
         item3 += 1;
         updateNumber();
+        updateNPC();
         updateItem3();
+        updateBuyButtons();
+    }
+}
+
+function buyItem4() {
+    if (number >= costItem4) {
+        number -= costItem4;
+        item4 += 1;
+        updateNumber();
+        updateNPC();
+        updateItem4();
+        updateBuyButtons();
+    }
+}
+
+function updateBuyButtons() {
+    // Update Buy Item1 button
+    const buyItem1Button = document.getElementById("buyItem1");
+    if (number >= costItem1) {
+        buyItem1Button.disabled = false;
+    } else {
+        buyItem1Button.disabled = true;
+    }
+    // Update Buy Item2 button
+    const buyItem2Button = document.getElementById("buyItem2");
+    if (number >= costItem2) {
+        buyItem2Button.disabled = false;
+    } else {
+        buyItem2Button.disabled = true;
+    }
+    // Update Buy Item3 button
+    const buyItem3Button = document.getElementById("buyItem3");
+    if (number >= costItem3) {
+        buyItem3Button.disabled = false;
+    } else {
+        buyItem3Button.disabled = true;
+    }
+    // Update Buy Item4 button
+    const buyItem4Button = document.getElementById("buyItem4");
+    if (number >= costItem4) {
+        buyItem4Button.disabled = false;
+    } else {
+        buyItem4Button.disabled = true;
+    }
+    // Update Buy increaseCPSButton1 button
+    const buyCPS1Button = document.getElementById("increaseCPSButton1");
+    if (number >= cpsItem1) {
+        buyCPS1Button.disabled = false;
+    } else {
+        buyCPS1Button.disabled = true;
+    }
+    // Update Buy increaseCPSButton2 button
+    const buyCPS2Button = document.getElementById("increaseCPSButton2");
+    if (number >= cpsItem2) {
+        buyCPS2Button.disabled = false;
+    } else {
+        buyCPS2Button.disabled = true;
+    }
+    // Update Buy increaseCPSButton3 button
+    const buyCPS3Button = document.getElementById("increaseCPSButton3");
+    if (number >= cpsItem3) {
+        buyCPS3Button.disabled = false;
+    } else {
+        buyCPS3Button.disabled = true;
+    }
+    // Update Buy increaseCPSButton4 button
+    const buyCPS4Button = document.getElementById("increaseCPSButton4");
+    if (number >= cpsItem4) {
+        buyCPS4Button.disabled = false;
+    } else {
+        buyCPS4Button.disabled = true;
+    }
+    // Update Buy increaseCPSButton5 button
+    const buyCPS5Button = document.getElementById("increaseCPSButton5");
+    if (number >= cpsItem5) {
+        buyCPS5Button.disabled = false;
+    } else {
+        buyCPS5Button.disabled = true;
     }
 }
 
 // Function to update the displayed Item1 count
+function updateNPC() {
+    npc = (1 + 1 * item1 + 7 * item2 + 40 * item3 + 1000 * item4);
+    document.getElementById("npc").innerText = npc;
+}
+function updateCPS() {
+    document.getElementById("cps").innerText = cps;
+}
 function updateItem1() {
     document.getElementById("item1").innerText = item1;
 }
@@ -74,6 +205,9 @@ function updateItem2() {
 }
 function updateItem3() {
     document.getElementById("item3").innerText = item3;
+}
+function updateItem3() {
+    document.getElementById("item4").innerText = item4;
 }
 
 // Function to add CPS to number every second
